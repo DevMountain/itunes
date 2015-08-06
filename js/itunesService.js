88bab9633c01c032681a8430b01eb6e8457b467b
Chris Glenn <topherblair@gmail.com>
2015-08-05 19:25:52 -0600

    still working on it

--- js/itunesService.js
+++ js/itunesService.js
@@ -2,7 +2,7 @@ var app = angular.module('itunes');
 
 app.service('itunesService', function($http, $q){
   //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
-  //Also note that we're using a 'service' and not a 'factory' so all your method you want to call in your controller need to be on 'this'.
+  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
@@ -10,5 +10,12 @@ app.service('itunesService', function($http, $q){
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-
+    this.filterName = function(artist){
+    	return $http({
+    		method: 'GET',
+    		url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+    	}).then(function(response){
+    		return(response);
+    	})
+    }
 });
